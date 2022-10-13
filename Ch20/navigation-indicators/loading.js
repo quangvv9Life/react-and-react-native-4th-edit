58fcd57a2f954989a39669dcb9d9ac169cae5bc8
Viet Quang Vu <vuvietquang90@gmail.com>
2022-10-13 21:02:51 +0700

    Indicating progress, measuring progress, exploring navigation indicators, step progress

---
+++ Ch20/navigation-indicators/loading.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
+import styles from "./styles";
+
+export default function loading(Wrapped) {
+  return function LoadingWrapper(props) {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }, []);
+
+    if (loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator size="large"
+          color="#0000ff"/>
+        </View>
+      );
+    } else {
+      return <Wrapped {...props} />;
+    }
+  };
+}
---
+++ Ch20/step-progress-new/Js-IndexOf.js
@@ -0,0 +1,3 @@
+const Array = [1, 2, 3, 2, 1];
+
+console.log(Array.indexOf(2));
